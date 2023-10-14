3aa9d6d090188f7dac8640c2d98574d8e47cbd88
jigs <jigyansh2468@gmail.com>
2023-10-14 13:38:45 +0530

    ok

--- app/Signup/page.js
+++ app/Signup/page.js
@@ -1,62 +1,105 @@
 "use client";
 import React, { useState } from "react";
-const passcond = () => {};
-const confirmpass = () => {};
+
 const Signup = () => {
+  const [input, setinput] = useState({
+    username: "",
+    email: "",
+    PhNum: "",
+    city: "",
+    DOB: "",
+    pass: "",
+    cnfrmpass: "",
+  });
+
+  const oninputchange = (e) => {
+    const { name, value } = e.target;
+    setinput({ ...input, [name]: value });
+  };
+
+  const checkcpass = () => {
+    if (input.pass !== input.cnfrmpass) {
+      alert("Confirm Password does not match Password.");
+    }
+  };
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    if (input.pass === input.cnfrmpass) {
+      console.log("Form submitted with matching passwords:", input);
+    } else {
+      alert("Password and Confirm Password do not match.");
+    }
+  };
+
   return (
     <>
-      <h1 className="text-center text-xl font-bold my-5 ">Signup FORM</h1>
+      <h1 className="text-center text-xl font-bold my-5">Signup FORM</h1>
       <div>
         <center>
-          <div className="flex items-center justify-center h-auto w-1/4 border-2 border-black px-20  rounded-xl lg:w-1/4 sm:w-1/4 ">
+          <div className="flex items-center justify-center h-auto w-1/4 border-2 border-black px-20 rounded-xl lg:w-1/4 sm:w-1/4">
             <form
-              action=""
+              onSubmit={handlesubmit}
               className="text-center flex-column items-center gap-5 mt-5"
             >
               <input
                 type="text"
-                placeholder="Name"
-                required
+                placeholder="UserName"
+                name="username"
+                value={input.username}
+                onChange={oninputchange}
                 className="my-5 p-2 rounded-md border-2 border-black"
+                required
               />
               <input
                 type="email"
                 placeholder="Email"
-                required
+                name="email"
+                value={input.email}
+                onChange={oninputchange}
                 className="my-5 p-2 rounded-md border-2 border-black"
+                required
               />
               <br />
               <input
                 type="number"
                 placeholder="Phone Number"
+                name="PhNum"
+                value={input.PhNum}
+                onChange={oninputchange}
                 className="my-5 p-2 rounded-md border-2 border-black"
+                required
               />
               <br />
               <input
                 type="text"
                 placeholder="City"
-                required
+                name="city"
+                value={input.city}
+                onChange={oninputchange}
                 className="my-5 p-2 rounded-md border-2 border-black"
+                required
               />
               <br />
               <input
-                type="date"
-                placeholder="DOB"
-                className="my-5 p-2 px-11 rounded-md border-2 border-black"
-              />
-              <input
                 type="password"
                 placeholder="Password"
-                value={pass}
-                onChange={passcond}
+                name="pass"
+                value={input.pass}
+                onChange={oninputchange}
                 className="my-5 p-2 rounded-md border-2 border-black"
+                required
               />
               <br />
               <input
                 type="password"
                 placeholder="Confirm Password"
-                onChange={confirmpass}
+                name="cnfrmpass"
+                value={input.cnfrmpass}
+                onBlur={checkcpass}
+                onChange={oninputchange}
                 className="my-5 p-2 rounded-md border-2 border-black mb-8"
+                required
               />
               <button className="mb-8 font-semibold text-lg border-2 border-zinc-300 rounded-lg px-10 p-2 hover:bg-pink-300 hover:text-white hover:cursor-pointer">
                 SignUp
