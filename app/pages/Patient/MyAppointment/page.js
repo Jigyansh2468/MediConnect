55999d7504906732648bf4bb41ba228660de60aa
jigyansh <jigyansh2468@gmail.com>
2023-10-28 16:13:34 +0530

    .

--- app/pages/Patient/MyAppointment/page.js
+++ app/pages/Patient/MyAppointment/page.js
@@ -1,16 +1,37 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import PatientDashboard from '@/Components/PatientDashboard'
-function MedicalRecords() {
+const MyAppointment = () => {
     const [list, setlist] = useState([])
+    useEffect(() => {
+        axios.get("", input, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            withCredentials: true,
+        })
+            .then((response) => { setlist(response.data) })
+            .catch((error) => { console.log(error) })
+    }, [])
     return (
         <>
             <PatientDashboard />
-            <div>
-
+            <div className="bg-blue-100 p-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {list.map((item, i) => (
+                        <div
+                            key={i}
+                            className="bg-white rounded p-4 shadow-md transition transform hover:scale-105"
+                        >
+                            <p>{item.title}</p>
+                            <p>{item.description}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     )
 }
 
-export default MedicalRecords
+export default MyAppointment
